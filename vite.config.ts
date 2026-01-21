baa643c8e72d741bc98704e88f41da0e77205d2e
Dennis2y <charles.dennis71@yahoo.com>
2026-01-21 01:23:03 +0100

    fix: add redirects for Render SPA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        { src: "public/_redirects", dest: "." }
+      ]
+    })
   ],
   build: {
     outDir: "build",

12279d9c7dbb00aa80eba218aaf73a5c9baae52b
Dennis2y <charles.dennis71@yahoo.com>
2026-01-21 01:38:10 +0100

    deploy: vercel-ready build

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,9 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), tsconfigPaths()],
   build: {
-    outDir: "build",
+    outDir: "dist",
     emptyOutDir: true,
   },
 });

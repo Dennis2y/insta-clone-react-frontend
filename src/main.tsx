0bff858735a1b7a6d05f5fa40de20a41ba0ada96
Dennis2y <charles.dennis71@yahoo.com>
2026-01-21 01:20:37 +0100

    fix: fully working Vite SPA router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "../react-router.config";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "../react-router.config";
 
-const root = document.getElementById("root")!;
-createRoot(root).render(<RouterProvider router={router} />);
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")!)
+  .render(<RouterProvider router={router} />);
